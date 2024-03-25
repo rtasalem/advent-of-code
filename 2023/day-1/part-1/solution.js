55d8a01f35e0ed09aaf03375220a6233836e44f7
Rana Salem <ranatasalem@gmail.com>
2024-03-25 16:02:00 +0000

    add current work

--- 2023/day-1/part-1/solution.js
+++ 2023/day-1/part-1/solution.js
@@ -2,7 +2,7 @@ const input = require('./input')
 
 let total = 0
 
-for (let line of input) {
+for (const line of input) {
   const numbers = line.split('').filter((element) => !isNaN(parseInt(element)))
   total += parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`)
 }
---
+++ 2023/day-1/part-2/solution.js
@@ -0,0 +1,15 @@
+// 2023 DAY 1 PART 2 - STILL TO COMPLETE :(
+const input = require('../part-1/input')
+
+const integerMap = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}
